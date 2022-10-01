1d40d9d0d58382c5efdcec2a50cddf9e92b5648d
kiran kumar <ch.kirankumar311@gmail.com>
2022-10-01 22:23:28 +0530

    cors

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ const express=require("express");
 
 const app =express();
 
+const cors=require("cors");
+
 app.use(express.json());
+
+app.use(cors());
 
 const registerController=require("./controller/register.controller");
 
