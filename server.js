546696247ea1bb7a62adf25362f6d6b790d10ff0
kiran kumar <ch.kirankumar311@gmail.com>
2022-10-02 12:18:52 +0530

    v

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ const app=require("./src/index");
 require("dotenv").config();
 const Port=process.env.port || 8080;
 const connect=require("./src/config/db")
-
+const mongoose=require("mongoose")
 
 
 app.listen(Port,async()=>{
     try {
+
+        mongoose.set('bufferCommands', false);
+
         await connect();
+
         console.log("connected to a port")
     } catch (error) {
         console.log({message:error.message})
